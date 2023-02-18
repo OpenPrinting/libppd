59b6d5a8480fc75a7f8d9ec77a3a7b886eb24edb
Biswadeep Purkayastha <98874428+metabiswadeep@users.noreply.github.com>
2023-02-19 05:16:12 +0530

    Removed bogus var

--- ppd/testppdfile.c
+++ ppd/testppdfile.c
@@ -33,7 +33,6 @@ int main(int argc,	    // I - Number of command-line args
   int relaxed;  // If relaxed mode is to be used 
   cups_array_t *file_array;  // Array consisting of filenames of the ppd files to be checked 
   int files;			   // Number of files 
-  cups_array_t	*output;  // Output array
   char *line;   // Looping var for output array
   int warn;			    // Which errors to just warn about 
   int ignore;			   // Which errors to ignore 
